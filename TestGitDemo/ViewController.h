516222802cbe5c65f1c265cb76605b6f0d8eb426
maomaotou <linyong@172-7-6-252.lightspeed.hstntx.sbcglobal.net>
2017-03-22 10:19:38 +0800

    1.5

---
+++ TestGitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGitDemo
+//
+//  Created by linyong on 2017/3/22.
+//  Copyright © 2017年 linyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demo/DemoShowTest.h
@@ -0,0 +1,15 @@
+//
+//  DemoShowTest.h
+//  TestGitDemo
+//
+//  Created by linyong on 2017/3/22.
+//  Copyright © 2017年 linyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemoShowTest : UIView
+
++ (void) showWithFrame:(CGRect) frame;
+
+@end
